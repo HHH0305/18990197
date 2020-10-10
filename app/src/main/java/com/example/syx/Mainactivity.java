857bf7112e7e1b6bd88e325e9963bbf0cb948062
b <xxxx>
2020-10-10 11:50:45 +0800

    第二次作业

---
+++ app/src/main/java/com/example/syx/Mainactivity.java
@@ -0,0 +1,13 @@
+package com.example.syx;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bbs);
+    }
+}
